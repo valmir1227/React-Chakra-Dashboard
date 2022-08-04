68d36d2da3282c6e4b7f032b11ea51c49d06e308
Valmir Almeida <almeidavalmir76@gmail.com>
2022-08-04 08:33:49 -0300

    :package: Header completed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,9 @@
-import { Flex, Icon, Input, Text } from "@chakra-ui/react";
-import { RiSearchLine } from "react-icons/ri";
+import { Flex, Icon, Input, HStack, Text, Box, Avatar } from "@chakra-ui/react";
+import {
+  RiNotificationLine,
+  RiSearchLine,
+  RiUserAddLine,
+} from "react-icons/ri";
 export function Header() {
   return (
     <Flex
@@ -39,6 +43,33 @@ export function Header() {
           _placeholder={{ color: "gray.400" }}
         />
         <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+      <Flex align="center" ml="auto">
+        <HStack
+          spacing="5"
+          pr="8"
+          mx="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Valmir ALmeida</Text>
+            <Text color="gray.300" fontSize="small">
+              almeidavalmir76@gmail.com
+            </Text>
+          </Box>
+          <Avatar
+            size="md"
+            name="valmir almeida"
+            src="https://avatars.githubusercontent.com/u/68277445?v=4"
+          />
+        </Flex>
       </Flex>
     </Flex>
   );
