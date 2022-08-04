a5fe3bb0cecc8f7507dd12e4058a4c8f873f123c
Valmir Almeida <almeidavalmir76@gmail.com>
2022-08-03 21:59:24 -0300

    :pencil: Page: Sing in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,60 @@
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+} from "@chakra-ui/react";
+
 export default function Home() {
   return (
     <div>
-      <h1>Ola</h1>
+      <Flex w="100vw" h="100vh" align="center" justify="center">
+        <Flex
+          as="form"
+          w="100%"
+          maxW={360}
+          bg="gray.800"
+          p={8}
+          borderRadius={8}
+          flexDir="column"
+        >
+          <Stack spacing={4}>
+            <FormControl>
+              <FormLabel htmlFor="email">E-mail</FormLabel>
+              <Input
+                type="email"
+                name="email"
+                focusBorderColor="pink.500"
+                bgColor="gray.900"
+                variant="filled"
+                _hover={{
+                  bgColor: "gray.900",
+                }}
+                size="lg"
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="password">Password</FormLabel>
+              <Input
+                type="password"
+                name="password"
+                focusBorderColor="pink.500"
+                bgColor="gray.900"
+                variant="filled"
+                _hover={{
+                  bgColor: "gray.900",
+                }}
+                size="lg"
+              />
+            </FormControl>
+          </Stack>
+          <Button type="submit" mt="6" colorScheme="pink" size="lg">
+            Enviar
+          </Button>
+        </Flex>
+      </Flex>
     </div>
   );
 }
