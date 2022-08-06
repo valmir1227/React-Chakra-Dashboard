b6a4a9290d76393ac5b5df916655c7c26779d7ae
Valmir Almeida <almeidavalmir76@gmail.com>
2022-08-06 07:43:01 -0300

    :heavy_check_mark: Add ApexOptions

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import dynamic from "next/dynamic";
 import { Flex, SimpleGrid, Box, Text, theme } from "@chakra-ui/react";
-//import { ApexOptions } from "apexcharts";
+import { ApexOptions } from "apexcharts";
 
 import { Header } from "../components/Header/Header";
 import Sidebar from "../components/Sidebar/Sidebar";
@@ -10,32 +10,43 @@ const Chart = dynamic(() => import("react-apexcharts"), {
   ssr: false,
 });
 
-const options = {
+const options: ApexOptions = {
   chart: {
     toolbar: { show: false },
-    zoom: { enable: false },
+    zoom: { enabled: false },
     foreColor: theme.colors.gray["500"],
   },
+
   grid: { show: false },
-  dataLabels: { enabled: false },
+  dataLabels: {
+    enabled: false,
+  },
+
   tooltip: { enabled: false },
+
   xaxis: {
-    type: "datatime",
+    type: "datetime",
     axisBorder: {
       color: theme.colors.gray["600"],
     },
-    axisTicks: { colors: theme.colors.gray["600"] },
+    axisTicks: { color: theme.colors.gray["600"] },
     categories: [
-      "2022-04-02T00:00:00:00.000Z",
-      "2022-04-03T00:00:00:00.000Z",
-      "2022-04-04T00:00:00:00.000Z",
-      "2022-04-05T00:00:00:00.000Z",
-      "2022-04-06T00:00:00:00.000Z",
-      "2022-04-07T00:00:00:00.000Z",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      // "2022-04-02T00:00:00:00.000Z",
+      //"2022-04-03T00:00:00:00.000Z",
+      //   "2022-04-04T00:00:00:00.000Z",
+      //"2022-04-05T00:00:00:00.000Z",
+      //   "2022-04-06T00:00:00:00.000Z",
+      //   "2022-04-07T00:00:00:00.000Z",
     ],
   },
+
   fill: {
-    opacity: 0.3,
     type: "gradient",
     gradient: {
       shade: "dark",
@@ -48,7 +59,7 @@ const options = {
 const series = [
   {
     name: "series1",
-    data: [122, 45, 78, 92, 12, 45],
+    data: [200, 150, 78, 92, 44, 66],
   },
 ];
 
