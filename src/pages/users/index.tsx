68990c9d32bccc510e8ed4509bf44fa17c5b2700
Valmir Almeida <almeidavalmir76@gmail.com>
2022-08-26 22:26:11 -0300

    Mirage Seeds and factories

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,6 +16,7 @@ import {
   theme,
 } from "@chakra-ui/react";
 import Link from "next/link";
+import { useEffect } from "react";
 import { RiAddLine, RiPencilLine, RiRefreshLine } from "react-icons/ri";
 import Header from "../../components/Header/Header";
 import Pagination from "../../components/Pagination";
@@ -26,6 +27,12 @@ export default function UserList() {
     base: false,
     lg: true,
   });
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/users")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
 
   return (
     <Box>
--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,12 +1,11 @@
-import { createServer, Model } from "miragejs"
+import { createServer, Factory, Model } from "miragejs"
+import { faker } from '@faker-js/faker';
 
 type User = {
     name: string
     email: string
-    creeated_at: string
+    created_at: string
 }
-
-
 
 export function makeServer() {
     const server = createServer({
@@ -14,9 +13,32 @@ export function makeServer() {
             user: Model.extend<Partial<User>>({})
         },
 
+        factories: {
+            user: Factory.extend({
+                name(i: number) {
+                    return `User${i + 1}`
+                },
+                email() {
+                    return faker.internet.email().toLocaleLowerCase()
+                },
+
+                created_at() {
+                    return faker.date.recent(10)
+                }
+            })
+        },
+        seeds(server) {
+            server.createList("user", 200)
+        },
+
+
+
+
+
         routes() {
             this.namespace = "api"
 
+            this.timing = 750
             this.get("/users")
             this.post("/users")
 
